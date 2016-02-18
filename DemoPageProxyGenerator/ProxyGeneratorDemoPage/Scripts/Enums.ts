27d7e0727bf2632d16a090fa7e59cf0342fecfdb
SquadWuschel <joecorporation@gmx.de>
2016-02-18 22:11:08 +0100

    test

--- DemoPageProxyGenerator/ProxyGeneratorDemoPage/Scripts/Enums.ts
+++
@@ -1,8 +0,0 @@
-module ProxyGeneratorDemoPage.Helper {
-	export const enum ClientAccess {
-		All = 0,
-		None = 1,
-		Admin = 2
-	}
-}
-
--- DemoPageProxyGenerator/ProxyGeneratorDemoPage/ScriptsApp/Services/homeAngularJsSrv.js
+++ DemoPageProxyGenerator/ProxyGeneratorDemoPage/ScriptsApp/Services/homeAngularJsSrv.js
@@ -1,6 +1,6 @@
 //Warning this file was dynamicly created.
 //Please don't change any code it will be overwritten next time the template is executed.
-//Created on 18.02.2016 time 22:02 from SquadWuschel.
+//Created on 18.02.2016 time 22:10 from SquadWuschel.
 
   function homeAngularJsSrv($http) { this.http = $http; } 
 
