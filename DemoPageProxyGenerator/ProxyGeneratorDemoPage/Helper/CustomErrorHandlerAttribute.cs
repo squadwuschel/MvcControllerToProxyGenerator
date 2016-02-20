916c66459307e234086e6ebb09df6b25d2f4e0ea
SquadWuschel <joecorporation@gmx.de>
2016-02-20 22:37:31 +0100

    Custom Error Handler Attribute

---
+++ DemoPageProxyGenerator/ProxyGeneratorDemoPage/Helper/CustomErrorHandlerAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace ProxyGeneratorDemoPage.Helper
+{
+    /// <summary>
+    /// Custom Error Handler der über der passenden Controller Funktion verwendet wird um unbehandelte Ausnahmen abzufangen.
+    /// </summary>
+    public class CustomErrorHandlerAttribute : HandleErrorAttribute
+    {
+        public bool IsAjaxCall { get; set; }
+
+        public override void OnException(ExceptionContext context)
+        {
+            if (IsAjaxCall)
+            {
+                context.ExceptionHandled = true;
+                var jsonResult = new JsonResult();
+                jsonResult.Data = new {Message = context.Exception.Message , MessageType = "Error"};
+                jsonResult.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+                jsonResult.ExecuteResult(context);
+            }
+            else
+            {
+                // if not an ajax request, continue with logic implemented by MVC -> html error page
+                base.OnException(context);
+            }
+        }
+    }
+}
